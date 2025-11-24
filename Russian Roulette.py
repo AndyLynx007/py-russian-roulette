08a0ef767896ec82e3e7ecdbc63c8c25764f8701
AndyLynx007 <januardwinagung@gmail.com>
2025-11-24 12:03:46 +0700

    First draft of russian roulette

---
+++ Russian Roulette.py
@@ -0,0 +1,17 @@
+import random
+pos_b = random.randint(0,5)
+pos_p = 0
+
+start = input("Press enter to start:")
+
+while quit != "q":
+    if pos_p == pos_b:
+        print("bang")
+        input("ur ded :(")
+        break
+    elif pos_p != pos_b:
+        print("click")
+        pos_p += 1
+        if pos_p > 5:
+            pos_p = 0
+        quit = input("again?")
